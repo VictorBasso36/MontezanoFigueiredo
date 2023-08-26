fd7d8bc11f3a559f6a0562d0bff7337b2bb749d7
VictorBasso36 <victorbasso36>
2023-08-26 19:17:27 -0300

    fix

--- src/app/Areas-de-Atuacao/Servico/[slug]/page.tsx
+++ src/app/Areas-de-Atuacao/Servico/[slug]/page.tsx
@@ -2,7 +2,6 @@
 "use client";
 import React from "react";
 import { gql, useQuery } from '@apollo/client';
-import { usePathname } from "next/navigation";
 
 
 export default function ServicoPage({ params }: { params: { slug: string } }) {
@@ -15,30 +14,6 @@ export default function ServicoPage({ params }: { params: { slug: string } }) {
         {/* Outras informações da área de atuação */}
       </div>
     );
-}
-
-export async function getStaticPaths({ params }: { params: { slug: string } }) {
-  const GET_ATUACOES = gql`
-    query Atuacao($atuacaoId: ID) {
-      atuacao(id: $atuacaoId) {
-        data {
-          id
-        }
-      }
-    }
-  `;
-
-  const { data } = useQuery(GET_ATUACOES, {
-    variables: { atuacaoId: params.slug },
-  });
-
-  const atuacaoId = data.atuacao?.data?.id;
-
-  if (atuacaoId !== null) {
-    return { paths: [], fallback: 'blocking' };
-  }
-
-  return { paths: [], fallback: false };
 }
 
 
