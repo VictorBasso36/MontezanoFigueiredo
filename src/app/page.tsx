d56f6e103201e1a96f0ee5b5264f334aad19bce6
VictorBasso36 <victorbasso36>
2023-08-26 15:26:42 -0300

    apollo query and more components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
     <>
     <Navbar/>
     <main className={styles.main}>
-      <Banner convertText='A Montezano Figueiredo é um Hub de inteligência que alinha a Gestão Tributária e Planejamento Estratégico. ' title='<h1>GESTÃO <strong>TRIBUTÁRIA</strong><span> &</span><br /> PLANEJAMENTO <strong>ESTRATÉGICO</strong></h1>'/>
+      <Banner />
     </main>
     <NoticiasLast/>
     <Convert/>
